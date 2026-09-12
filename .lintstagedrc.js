2bc3eb3edecd82d311e5195441e8ed4f4ff4e5f1
Joseph Khoury <joseph-khoury@outlook.com>
2026-09-12 16:54:44 +0300

    chore: add DX setup for formatting and commit checks

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+import path from 'node:path'
+
+const buildEslintCommand = (filenames) =>
+  `eslint --fix ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`
+
+export default {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
+  '!(*.{js,jsx,ts,tsx})': ['prettier --write --ignore-unknown'],
+}
